532ef926450a9ddf3c451a2abb6438896a8a23ee
forLoop94 <charlesuwem94@gmail.com>
2023-10-18 17:16:21 +0100

    Add Tailwind, dependencies and config to project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import postcss from 'vite-plugin-postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // postcss(),
+  ],
 })
